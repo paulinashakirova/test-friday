03188d91fc69911d8c564063b1c679772b5c506d
Paulina Shakirova <paulinashakirova@gmail.com>
2023-02-21 21:35:23 +0100

    feat: introduce category pill/tag/button

---
+++ components/CategoryTag.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { computed } from '@nuxtjs/composition-api'
+// interface Colors {
+//   purple?: ''
+//   pink?: ''
+//   orange?: ''
+//   blue?: ''
+//   yellow?: ''
+//   lightGray?: ''
+//   darkGray?: ''
+// }
+
+interface Props {
+  label: string
+  color: string
+}
+
+const props = defineProps<Props>()
+
+const dynamicColor = computed(() => {})
+</script>
+
+<template>
+  <div class="bg-purple-300 py-3 px-4 w-fit rounded-md">
+    {{ props.label }}
+  </div>
+</template>
