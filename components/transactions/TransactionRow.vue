ddefe5a4be47a8aac51d932ef9efadcc0fdbf919
Paulina Shakirova <paulinashakirova@gmail.com>
2023-02-23 13:37:48 +0100

    feat: add categories+banks to table+Date format

--- components/transactions/TransactionRow.vue
+++ components/transactions/TransactionRow.vue
@@ -1,29 +1,40 @@
 <script setup lang="ts">
+import CategoryTag from '@/components/category-tag/CategoryTag.vue'
 import { computed } from '@nuxtjs/composition-api'
 
 interface Props {
   reference?: string
   category?: string
-  date: string // TO DO: make it a Date
+  date: string
   currency: string
   amount: number
+  bank?: string
+  color?: string | number
 }
 
 const props = withDefaults(defineProps<Props>(), {
   reference: 'No reference provided',
 })
+
 const reference = computed(() => {
   return !props.reference ? 'No reference provided' : props.reference
+})
+
+const date = computed(() => {
+  const date = new Date(props.date)
+  return date.toLocaleDateString()
 })
 </script>
 
 <template>
   <div
-    class="grid grid-cols-4 border-b-2 py-3 border-gray-100 text-gray-500 text-sm"
+    class="grid grid-cols-5 border-b-2 py-3 border-gray-100 text-gray-500 text-sm"
   >
     <div :class="!props.reference ? 'text-gray-300' : ''">{{ reference }}</div>
-    <div class="">{{ props.category }}</div>
-    <div class="">{{ props.date }}</div>
-    <div class="">{{ props.amount }} {{ props.currency }}</div>
+    <CategoryTag :label="props.category" :color="props.color" />
+
+    <div>{{ props.bank }}</div>
+    <div>{{ date }}</div>
+    <div>{{ props.amount }} {{ props.currency }}</div>
   </div>
 </template>
--- sections/TransactionsTable.vue
+++ sections/TransactionsTable.vue
@@ -1,17 +1,36 @@
 <script setup lang="ts">
 import TransactionRow from '@/components/transactions/TransactionRow.vue'
+import accounts from '@/static/mock-data/accounts.json'
+import categories from '@/static/mock-data/categories.json'
 import transactions from '@/static/mock-data/transactions.json'
+
+const categoryById = (id: string) => {
+  const category = categories.find((category) => category.id === id)
+  return category?.name
+}
+
+const categoryColorById = (id: string) => {
+  const category = categories.find((category) => category.id === id)
+  console.log('color', category?.color)
+
+  return category?.color
+}
+
+const bankById = (id: string) => {
+  return accounts.find((account) => account.id === id)?.bank
+}
 </script>
 
 <template>
   <div>
     <div
-      class="grid grid-cols-4 border-b-2 border-t-2 py-2 border-gray-200 text-gray-500 text-sm"
+      class="grid grid-cols-5 border-b-2 border-t-2 py-2 border-gray-200 text-gray-500 text-sm"
     >
-      <div class="">Reference</div>
-      <div class="">Category</div>
-      <div class="">Date</div>
-      <div class="">Amount</div>
+      <div>Reference</div>
+      <div>Category</div>
+      <div>Bank</div>
+      <div>Date</div>
+      <div>Amount</div>
     </div>
     <div
       v-for="{
@@ -27,10 +46,12 @@ import transactions from '@/static/mock-data/transactions.json'
     >
       <TransactionRow
         :reference="reference"
-        category="tbc"
+        :category="categoryById(categoryId)"
+        :bank="bankById(accountId)"
         :date="date"
         :currency="currency"
         :amount="amount"
+        :color="categoryColorById(categoryId)"
       />
     </div>
   </div>
