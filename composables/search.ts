b2d47652aad47e37fe036be2838e35b9bfc6f9fd
Paulina Shakirova <paulinashakirova@gmail.com>
2023-02-26 19:35:54 +0100

    refactor: improve code, refactor a bit

--- composables/search.ts
+++ composables/search.ts
@@ -5,3 +5,16 @@ export const performSearch = (searchString: string, transactions: any) => {
       )
     : transactions
 }
+
+export const searchBankById = (id: string, accounts: any) => {
+  return accounts.find((account: any) => account.id === id)?.bank
+}
+
+export const searchCategoryById = (id: string, categories: any) => {
+  const category = categories.find((category: any) => category.id === id)
+  return category?.name
+}
+
+export const searchCategoryColorById = (id: string, categories: any) => {
+  return categories.find((category: any) => category.id === id)?.color
+}
