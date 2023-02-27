58072591d902992a50991f70f8d09133af0b7453
Paulina Shakirova <paulinashakirova@gmail.com>
2023-02-27 22:52:05 +0100

    feat: mock async fetch data using composables

---
+++ composables/use-accounts.ts
@@ -0,0 +1,23 @@
+import accounts from '@/static/mock-data/accounts.json'
+import { Account } from '@/types/accounts'
+
+export const fetchAllAccounts = async () => {
+  const mockAsyncPromise = (mockAccounts: Account[], delay: number) =>
+    new Promise((resolve, reject) =>
+      setTimeout(
+        () =>
+          mockAccounts
+            ? resolve(mockAccounts)
+            : reject(
+                new Error(
+                  "Sadly, we couldn't load the necessary data. Please try again, or contact us. Sorry for the inconvenience."
+                )
+              ),
+        delay
+      )
+    )
+
+  return mockAsyncPromise(accounts, 1000)
+
+  // catch is unnecesary here, since it is duplicating an error displayed in the console (should error occur)
+}
