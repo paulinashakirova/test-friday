cf4280ca15caec2e8f35dc2b2924285d81da9342
Paulina Shakirova <paulinashakirova@gmail.com>
2023-03-20 11:09:00 +0100

    feat: integrate supabase

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,29 @@
 <script setup lang="ts">
-import TransactionsTable from '@/sections/TransactionsTable.vue'
+import { supabase } from '@/lib/supabaseClient';
+import { onMounted, ref } from '@nuxtjs/composition-api';
+// import TransactionsTable from '@/sections/TransactionsTable.vue'
+
+const transactions = ref([]) as any;
+
+async function getTransactions() {
+  let { data: Account, error } = await supabase.from('Account').select('*');
+
+  transactions.value = Account;
+}
+
+onMounted(() => {
+  getTransactions();
+});
 </script>
 
 <template>
   <div class="mx-10 mt-10">
-    <TransactionsTable />
+    <h1>Hello world</h1>
+    <ul>
+      <li v-for="transaction in transactions" :key="transaction.accountId">
+        {{ transaction }}
+      </li>
+    </ul>
+    <!-- <TransactionsTable /> -->
   </div>
 </template>
